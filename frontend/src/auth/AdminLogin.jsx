aa6c2e3849af78984510d6f0e9dd92e6577fe3c5
S DEEPAKRAJ <deepakaero2000@gmail.com>
2025-07-03 20:41:59 +0530

    Update AdminLogin.jsx

--- frontend/src/auth/AdminLogin.jsx
+++ frontend/src/auth/AdminLogin.jsx
@@ -1,6 +1,7 @@
-import {useState} from 'react'
+
 import { useNavigate } from "react-router-dom"
 import axios from "axios";
+import { useEffect, useState } from 'react';
 import { useAuth } from './authProvider';
 import logo1 from '../assets/nouuu.png';
 const LoginPage=()=>{
